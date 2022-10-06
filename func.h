ae67bff0cf686460565faee9e3255629c1a09be6
Justas321 <113771423+Justas321@users.noreply.github.com>
2022-10-06 20:48:46 +0300

    Delete func.h

--- func.h
+++
@@ -1,4 +0,0 @@
-double vid_med (char tikrinimas, vector <duom> stud, int laikinas, int kiek);
-void Nuskaitymas(vector <duom> &stud, int &kiek, char tikrinimas);
-void Isvedimas(vector <duom> stud, int kiek, char tikrinimas);
-bool Palyginimas(duom pirmas, duom antras);
